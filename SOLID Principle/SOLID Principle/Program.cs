fd76fc09a7ef400a975bc75fbf94c35e3603a62b
ntsige <natnael.tsige@wsu.edu>
2022-11-11 16:21:38 -0800

    fist draft.

---
+++ SOLID Principle/SOLID Principle/Program.cs
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Natnael Tsige">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+// Natnael Tsige
+
+namespace Spreadsheet_Natnael_Tsige
+{
+    using Single_Responsibility_Principle;
+
+    /// <summary>
+    /// Auto generated calss. The main enatry to the slotion.
+    /// </summary>
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
